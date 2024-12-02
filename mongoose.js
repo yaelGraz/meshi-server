20f5ba7158b243b3233694d7e995f75622e4edfe
yaelGraz <116269760+yaelGraz@users.noreply.github.com>
2024-12-02 20:47:32 +0200

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
-const uri="mongodb://localhost:27017/meshiDB";
-//const uri="mongodb+srv://ah0534172214:tvuch7221@cluster0.64malpb.mongodb.net/Meshi";
+//const uri="mongodb://localhost:27017/meshiDB";
+//const uri="mongodb+srv://yaelig053:<yaelig053>@cluster0.0lh8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//const dbURI = "mongodb+srv://yaelig053:<yaelig053>@cluster0.0lh8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri="mongodb+srv://ah0534172214:tvuch7221@cluster0.64malpb.mongodb.net/Meshi";
 
-const connectDB=async()=>{
-    await mongoose.connect(uri);
+const connectDB=async()=>{await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('MongoDB connection error:', err));
 }
+// const connectDB=async()=>{
+//     await mongoose.connect(uri);
+// }
 
 mongoose.set('toJSON',{
     virtuals:true,
@@ -15,4 +24,12 @@ mongoose.set('toJSON',{
 })
 
 const database=mongoose.connection;
-export default connectDB; 
+
+database.on('error',(error)=>{
+    console.log(error);
+})
+
+database.once('connected',()=>{
+    console.log('Database Connected');
+})
+export default connectDB;
