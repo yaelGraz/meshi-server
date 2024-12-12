9a733b69913604ec481dd52b5cc1cee80f032aa3
Yael <yaelig053@gmail.com>
2024-12-12 18:15:07 +0200

    1_try

---
+++ Routers/CategoriesRouter.js
@@ -0,0 +1,24 @@
+// import  express  from "express";
+// import CategoriesController from "../Controllers/CategorisController.js";
+
+// const CategoriesRouter=express.Router();
+
+// CategoriesRouter.get("/",CategoriesController.getList);
+
+// export default CategoriesRouter;
+import  express  from "express";
+import CategoriesController from "../Controllers/CategorisController.js";
+
+const CategoriesRouter=express.Router();
+
+CategoriesRouter.get('/',CategoriesController.getList);
+CategoriesRouter.get('/:subcategoryName',CategoriesController.getCategoryBySubcategory);
+
+CategoriesRouter.post('/add',CategoriesController.add);
+CategoriesRouter.put('/update/:id',CategoriesController.updateCategory)
+CategoriesRouter.put('/delete/:id',CategoriesController.deleteCategory)
+
+
+
+
+export default CategoriesRouter;
