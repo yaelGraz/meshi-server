d37684df6160df286e1bd927eec37abb0faf3a2c
Yael <yaelig053@gmail.com>
2024-12-18 19:56:28 +0200

    23

--- multerConfig.js
+++ multerConfig.js
@@ -1,13 +1,25 @@
-// multerConfig.js
 import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
-const storage = multer.memoryStorage();
-const upload = multer(console.log("multer"),{
-  
-  storage: storage,
-  limits: { fileSize: 5000 * 1024 * 1024 }, // Set the maximum file size to 50 MB (or any desired limit)
+// Define storage configuration for disk
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Set the directory where the file will be uploaded
+    const uploadDir = path.join(__dirname, '../uploads/'); // Update as needed
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir); // Set the destination folder
+  },
+  filename: (req, file, cb) => {
+    // Set the filename of the uploaded file
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
 });
 
-
+const upload = multer({ storage: storage });
 
 export default upload;
+
