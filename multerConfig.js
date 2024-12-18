206ecd1c069b531cb306930ded57192033d616ad
Yael <yaelig053@gmail.com>
2024-12-18 20:00:13 +0200

    24

--- multerConfig.js
+++ multerConfig.js
@@ -1,11 +1,16 @@
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';  // Import fileURLToPath
+import { dirname } from 'path';   
 
 // Define storage configuration for disk
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // Set the directory where the file will be uploaded
+       // Equivalent to __dirname in ES Modules
+       const __filename = fileURLToPath(import.meta.url);
+       const __dirname = dirname(__filename);
     const uploadDir = path.join(__dirname, '../uploads/'); // Update as needed
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
